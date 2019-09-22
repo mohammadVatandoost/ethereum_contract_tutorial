aa49f6e8b69508c4913b45448866f86eafbd27f5
mohammadvatandoost <mohamadvatandoost512@gmail.com>
2019-09-22 10:45:33 +0330

    add fininalize request

--- Campaign/contracts/campaign.sol
+++ Campaign/contracts/campaign.sol
@@ -6,14 +6,18 @@ contract Campaign {
      uint value;
      address recipient;
      bool complete;
+     uint approvalCount;
+     mapping(address => bool) approvals;
    }  
    // int[] storage myArray = numbers // myArray is pointer
    // int[] memory myArray = numbers // myArray is new variable
 
    address public manager;
    uint public minimunContribution;
-   address[] approvers;
+   // address[] approvers;
+   mapping(address => bool) public approvers;
    Request[] requests;
+   uint public approversCount;
 
    modifier restricted() {
       require(msg.sender == manager);
@@ -28,19 +32,40 @@ contract Campaign {
    function contribute() public payable {
    	// for validation
    	require(msg.value >= minimunContribution);
-      approvers.push(msg.sender);
+      // approvers.push(msg.sender);
+      approvers[msg.sender] = true;
+      approversCount++;
    } 
 
    function createRequest(string description, uint value, address recipient) public restricted { // restricted
+      // require(approvers[msg.sender]);
       // global variable just be storage
+      // just inintialize value type // do not initilize mapping
    	Request memory newRequest = Request({
          description: description,
          value: value,
          recipient: recipient,
-         complete: false
+         complete: false,
+         approvalCount: 0         
       }); 
 
       requests.push(newRequest); 
+   }
+
+   function approveRequest(uint index) public {
+     // Request storage request = requests[index];
+     require(approvers[msg.sender]);
+     require(!requests[index].approvals[msg.sender]);
+     requests[index].approvals[msg.sender] = true;
+     requests[index].approvalCount++;
+   }
+
+   function finalizeRequest(uint index) public restricted { 
+      // Request storage request = requests[index];
+      require(!requests[index].complete);
+      require(requests[index].approvalCount > (approversCount/2) );
+      requests[index].recipient.transfer(requests[index].value);
+      requests[index].complete = true;
    }
 
 }
